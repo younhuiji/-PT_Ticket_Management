53957ca24fe302ac407183c1f8175e1cf2bf1352
younhuiji <younhuiji@github.com>
2023-03-05 00:13:18 +0900

    Batch 수업 종료 후 이용권 차감

--- src/main/java/com/fastcampus/pass/repository/booking/BookingEntity.java
+++ src/main/java/com/fastcampus/pass/repository/booking/BookingEntity.java
@@ -1,6 +1,8 @@
 package com.fastcampus.pass.repository.booking;
 
 import com.fastcampus.pass.repository.BaseEntity;
+import com.fastcampus.pass.repository.pass.PassEntity;
+import com.fastcampus.pass.repository.user.UserEntity;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -28,5 +30,13 @@ public class BookingEntity extends BaseEntity {
     private LocalDateTime startedAt;
     private LocalDateTime endedAt;
     private LocalDateTime cancelledAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", insertable = false, updatable = false)
+    private UserEntity userEntity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "passSeq", insertable = false, updatable = false)
+    private PassEntity passEntity;
 
 }
--- src/main/java/com/fastcampus/pass/repository/pass/PassRepository.java
+++ src/main/java/com/fastcampus/pass/repository/pass/PassRepository.java
@@ -1,7 +1,15 @@
 package com.fastcampus.pass.repository.pass;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import javax.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 public interface PassRepository extends JpaRepository<PassEntity, Integer> {
-
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE PassEntity p" +
+            "          SET p.remainingCount = :remainingCount," +
+            "              p.modifiedAt = CURRENT_TIMESTAMP" +
+            "        WHERE p.passSeq = :passSeq")
+    int updateRemainingCount(Integer passSeq, Integer remainingCount);
 }
