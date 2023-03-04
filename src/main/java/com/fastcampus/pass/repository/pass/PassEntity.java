ade906d65425cec4981a0c8026c57c2f4f3c3875
younhuiji <younhuiji@github.com>
2023-03-04 22:04:04 +0900

    Batch 이용 기간에 따른 만료

--- src/main/java/com/fastcampus/pass/repository/pass/PassEntity.java
+++ src/main/java/com/fastcampus/pass/repository/pass/PassEntity.java
@@ -15,11 +15,12 @@ import java.time.LocalDateTime;
 @Table(name = "pass")
 public class PassEntity extends BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본 키 생성을 DB에 위임합니다. (AUTO_INCREMENT)
     private Integer passSeq;
     private Integer packageSeq;
     private String userId;
 
+    @Enumerated(EnumType.STRING)
     private PassStatus status;
     private Integer remainingCount;
 
---
+++ src/main/java/com/fastcampus/pass/repository/pass/PassRepository.java
@@ -0,0 +1,7 @@
+package com.fastcampus.pass.repository.pass;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PassRepository extends JpaRepository<PassEntity, Integer> {
+
+}
