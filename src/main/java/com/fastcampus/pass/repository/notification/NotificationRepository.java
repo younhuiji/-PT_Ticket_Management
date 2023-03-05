897bc3091bc37672d38e5ecdfa371de7835eae33
younhuiji <younhuiji@github.com>
2023-03-05 13:18:38 +0900

    Batch 예약된 수업 전 알람

---
+++ src/main/java/com/fastcampus/pass/repository/notification/NotificationRepository.java
@@ -0,0 +1,7 @@
+package com.fastcampus.pass.repository.notification;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<NotificationEntity, Integer> {
+}
+
